fab0893f1a23d719c23a98c2ebc68438bb3d2ffe
Emanuell Santos <dfmanu06@gmail.com>
2019-07-11 19:56:53 -0300

    Novo component + logica do grid

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,11 +5,12 @@ import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
     declarations: [ 
         PhotoComponent, 
-        PhotoListComponent, PhotoFormComponent 
+        PhotoListComponent, PhotoFormComponent, PhotosComponent 
     ],
     imports: [ 
         HttpClientModule,
