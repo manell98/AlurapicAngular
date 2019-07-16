2c231c8e056d47b73ee7b96d60a258ebaf8610e1
Emanuell Santos <dfmanu06@gmail.com>
2019-07-16 17:19:16 -0300

    ViewChild

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/core/auth.service';
 import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -9,6 +10,7 @@ import { Router } from '@angular/router';
 export class SigninComponent implements OnInit {
     
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
     
     constructor(
         private formBuilder: FormBuilder,
@@ -36,6 +38,7 @@ export class SigninComponent implements OnInit {
                 err => {
                     console.log(err);
                     this.loginForm.reset();
+                    this.userNameInput.nativeElement.focus();
                     alert('Invalid user name or password!')
                 }          
             );       
