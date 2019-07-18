221cfa23f15512da56836bd08a847db941bb5863
Emanuell Santos <dfmanu06@gmail.com>
2019-07-18 14:20:09 -0300

    Componente de registro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { SigninComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
     declarations: [
-        SigninComponent
+        SigninComponent,
+        SignupComponent
     ],
     imports: [
         ReactiveFormsModule,
         CommonModule,
         VMessageModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ]
 })
 export class HomeModule {}
